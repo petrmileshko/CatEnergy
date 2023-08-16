6f62f2e5f13e14898d14b7353a57d1c32f121d0c
Peter <mileshko.petr@yandex.ru>
2023-08-16 12:52:35 +0300

    updated gulpfile
    Настройка сборщика gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,12 @@ const sass = require('gulp-sass')(require('sass')); // препроцессор 
 const postcss = require('gulp-postcss'); // обработчик файла стилей
 const rename = require("gulp-rename"); // переименование файла
 const csso = require("postcss-csso"); // минификация файла стилей
+const terser = require("gulp-terser"); // обработка Js файлов
 const autoprefix = require('autoprefixer'); // автоматическая подстановка префиксов для поддержки разных браузеров
+const squoosh = require("gulp-libsquoosh"); // Оптимизация картинок
+const webp = require("gulp-webp");          // Конвертер изображений в формат webp
+const svgstore = require("gulp-svgstore");  // Сборка svg файлов в спрайт
+const del = require("del");  // удаление папки
 const sync = require('browser-sync').create();
 
 
@@ -16,8 +21,7 @@ const sync = require('browser-sync').create();
  * Задачи
  *
  */
-
-// Запуск сервера
+// Запуск сервера (режим разработчика)
 const server = (done) => {
   sync.init({
     server: {
@@ -29,62 +33,135 @@ const server = (done) => {
   });
   done();
 }
-
 exports.server = server;
 
-// Перезагрузка в случае изменений в файлах
-
-const reload = (done) => {
-  sync.reload();              // перезагрузка в случае если произошли изменения в исходных файлах
-  done();
-}
-
-// Отслеживание изменений в исходных файлах
-
+// Отслеживание изменений в исходных файлах (режим разработчика)
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series(styles)); // отслеживать файлы препроцессора
-  //gulp.watch("source/js/**/*.js", gulp.series(scripts));  // отслеживать файлы скриптов
- // gulp.watch("source/*.html", gulp.series(html, reload)); // отслеживать файлы разметки HTML
+  gulp.watch("source/js/**/*.js", gulp.series(scriptsjs));  // отслеживать файлы скриптов
+ // gulp.watch("source/*.html", gulp.series(copy, reload)); // отслеживать файлы разметки HTML
 }
-
 exports.watcher = watcher;
 
-//Генерация стилей из препроцессора
+// Перезагрузка в случае изменений в файлах (режим разработчика)
+const reload = (done) => {
+  sync.reload();              // перезагрузка в случае если произошли изменения в исходных файлах разметки
+  done();
+}
+exports.reload = reload;
 
+//Генерация файла стилей из файлов препроцессора
 const styles = () => {
   return gulp.src('source/sass/style.scss') // Загружаем главный препроцесорный файл
   .pipe(plumber()) // подключаем обработчик ошибок
   .pipe(sourcemap.init()) // Фиксация исходного состояния кода в файлах препроцессора
   .pipe(sass()) // запускаем препроцессор чтобы получить файл стилей
-  .pipe(postcss(
+  .pipe(postcss(    // полученный результат обработаем плагинами постпроцессора
     [
-      autoprefix(),
-      csso()
+      autoprefix(),   // добавить префиксы для поддержки разных версий браузеров
+      csso()          // минифицировать код CSS
     ]
   ))
-  .pipe(rename("style.min.css"))
-  .pipe(sourcemap.write("."))
-  .pipe(gulp.dest('build/css'))
+  .pipe(rename("style.min.css"))  // переименовать файл стилей
+  .pipe(sourcemap.write("."))     // сохранение карты стилей в отдельном файле в той же папке
+  .pipe(gulp.dest('build/css'))  // сохранение результатов сборки в указанной папке
+  .pipe(sync.stream());
 }
-
 exports.styles = styles;
 
+// Обработка скриптов JS
+const scriptsjs = () => {
+  return gulp.src("source/js/app.js")
+  .pipe(terser())               // обработчик файла Js скриптов
+  .pipe(rename("app.min.js"))   // переименовать файл
+  .pipe(gulp.dest("build/js")) // сохранение результатов в указанной папке
+  .pipe(sync.stream());
+}
+exports.scriptsjs = scriptsjs;
+
+// Удаление старой версии сборки перед записью новой
+const cleanbuild = ()=> {
+  return del("build");
+}
+exports.cleanbuild = cleanbuild;
+
+// Копирование файлов необходимых для нормального показа страницы в браузере
+const copy = (done) => {
+  return gulp.src([
+    "source/fonts/*.{woff2,woff}",    // копируем только шрифты указанного в {} формата
+    "source/*.ico",                   // копируем favicon
+    "source/*.webmanifest",           // копируем манифест с favicons
+    "source/img/**/*.svg",            // копируем файлы формата SVG
+    "source/*.html"                   // копируем разметку HTML
+  ],
+   {
+    base: "source"
+   })
+   .pipe(gulp.dest("build"));         // помещаем все файлы сборки в указанную папку
+   done();
+}
+exports.copy = copy;
+
+// Копирование медиа файлов без сжатия в режиме разработчика
+const copyImages =() => {
+  return gulp.src("source/img/**/*.{jpg,png}")  // копируем только файлы указанного в {} формата
+  .pipe(gulp.dest("build/img"));
+}
+exports.copyImages = copyImages;
+
+// Оптимизация и копирование медиа для сборки конечного продукта
+const optimizeImages = ()=> {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")  // выбираем все файлы указанного формата
+  .pipe(squoosh())                                  // пропускаем через плагин оптимизации
+  .pipe(gulp.dest("build/img"));                    // сохраняем в указанную папку
+}
+exports.optimizeImages = optimizeImages;
+
+// Конвертация изображений в формат webp для сборки конечного продукта
+const createWebp = ()=> {
+  return gulp.src("source/img/**/*.{jpg,png}")      // выбираем все файлы указанного формата
+  .pipe(webp({quality: 90}))                        // конвертируем через плагин
+  .pipe(gulp.dest("build/img"));                    // сохраняем в указанную папку
+}
+exports.createWebp = createWebp;
+
+//  Создание спрайта из иконок в svg
+const sprite = () => {
+  return gulp.src("source/img/icons/*.svg")   // выбираем все файлы формата SVG в папке с иконками
+    .pipe(svgstore({                          // создаем спрайт
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))               // Даем имя файлу
+    .pipe(gulp.dest("build/img"));            // сохраняем в указанную папку
+}
+exports.sprite = sprite;
 
 /**
  *  Запуск задач
  */
-
-// Запуск для сборки конечного продукта
+// Запуск для сборки конечного продукта ( npm run build )
 const build = gulp.series(
-      styles
-    );
+  cleanbuild,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    scriptsjs,
+    sprite,
+    createWebp
+  ));
 exports.build = build;
 
-// Запуск по умолчанию - сборка в режиме разработчика
-
+// Запуск по умолчанию - сборка в режиме разработчика (npm start)
 exports.default = gulp.series(
+  cleanbuild,
+  copy,
+  copyImages,
   gulp.parallel(
-    styles
+    styles,
+    scriptsjs,
+    sprite,
+    createWebp
   ),
   gulp.series(
     server,
